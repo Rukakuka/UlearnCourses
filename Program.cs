b4693d262ac4254dc4f98161961bf405e7cdfc88
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-02 01:08:30 +1000

    First commit
    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ULearnCourses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lesson13.Task2.ArrayExample();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
