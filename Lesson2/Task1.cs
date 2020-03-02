1199aaf0972c1c81a4029dab053b3856d342af25
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-02 16:22:30 +1000

    Update

---
+++ Lesson2/Task1.cs
@@ -0,0 +1,32 @@
+﻿// <Summary>
+// Очепятки
+// Вася нашел крутое приложение на телефон.Можно фотографировать текст и преобразовывать фотографию в текстовый документ.
+// Вася тут же сфотографировал код с экрана своего одногруппника и с сожалением обнаружил, что результат не компилируется.
+// По-видимому, некоторые символы распознались неверно.
+//Исправьте все ошибки так, чтобы код работал правильно.
+// </Summary>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ULearnCourses.Lesson2
+{
+    class Task1
+    {
+        public static void VasyaOchepyatka()
+        {
+            /*
+            Console.WriteLine{ 'Hello, World!'};
+            var numher = 5,5;
+            number += 7;
+            Console.Writeline(number):
+            */
+            Console.WriteLine( "Hello, World!");
+            var number = 5.5;
+            number += 7;
+            Console.WriteLine(number);
+        }
+
+    }
+}
---
+++ Lesson2/Test1.cs
@@ -0,0 +1,24 @@
+﻿// <Summary>
+// Ошибки компиляции - тест
+// </Summary>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ULearnCourses.Lesson2
+{
+	
+	class Program
+	{
+		static void Main()
+		{
+			System.Console.WriteLine(Min(4, 2, 3));
+		}
+
+		static int Min(int a, int b, int c)
+		{
+			return Math.Min(a, Math.Min(b, c));
+		}
+	}
+}
