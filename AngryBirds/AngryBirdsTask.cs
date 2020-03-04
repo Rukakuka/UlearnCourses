f32d6cf7ea9da18aaf93bf72a4bb2511c1af0ad1
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-04 10:35:31 +1000

    Update

--- AngryBirds/AngryBirdsTask.cs
+++ AngryBirds/AngryBirdsTask.cs
@@ -4,6 +4,8 @@ namespace AngryBirds
 {
 	public static class AngryBirdsTask
 	{
+		private const double g = 9.81;
+		private const double pi = 3.1415926535;
 		// Ниже — это XML документация, её использует ваша среда разработки, 
 		// чтобы показывать подсказки по использованию методов. 
 		// Но писать её естественно не обязательно.
@@ -12,7 +14,9 @@ namespace AngryBirds
 		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
 		public static double FindSightAngle(double v, double distance)
 		{
-			return Math.PI / 4;
+			// S = v0^2/g * sin(2*alpha) =>
+			double alpha = 0.5 * Math.Asin(distance * g / (Math.Pow(v, 2)));
+			return alpha < 0 ? Double.NaN : alpha < pi / 2 ? alpha : Double.NaN;
 		}
 	}
 }
