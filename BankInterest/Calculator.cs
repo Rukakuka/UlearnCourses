1cd963147c3194151de6979bc0068fdaa35b9be6
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-04 15:31:45 +1000

    Update

--- BankInterest/Calculator.cs
+++ BankInterest/Calculator.cs
@@ -6,9 +6,35 @@ namespace BankInterest
 {
     public static class Calculator
     {
-        public static  CalculatePercentage()
+        public static double Calculate(string userInput)
         {
+            string[] depositInfo = userInput.Split(' ');
 
+            double amount = Double.Parse(depositInfo[0]);
+            double percent = Double.Parse(depositInfo[1]);
+            int months = Int32.Parse(depositInfo[2]);
+
+            if (amount <= 0 || percent < 0 || months < 0)
+                throw new ArgumentException();
+            if (months == 0)
+                return amount;
+
+            double deposit = Calculate(amount, percent, months);
+            return deposit;
+        }
+
+        private static double Calculate(double amount, double percent, int month)
+        {
+            amount += amount * ((double)1 / 12) * (percent / 100);
+            if (month != 1)
+            {
+                // step in till we hit the first month
+                amount = Calculate(amount, percent, month - 1);
+                return amount;
+            }
+            else
+                // then return from the deep
+                return amount;
         }
     }
 }
--- BankInterest/Program.cs
+++ BankInterest/Program.cs
@@ -6,7 +6,12 @@ namespace BankInterest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Please enter your deposit info, delimiting it by spaces: Initial amount of money," +
+                "then deposit percent, and then time span in months");
+            var str = Console.ReadLine();
+            var deposit = Calculator.Calculate(str);
+            Console.WriteLine(deposit);
         }
     }
 }
