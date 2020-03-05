d9233c71821a540d9fd8a9c786e5ad9bb673b7c8
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-05 23:27:57 +1000

    Update

--- Rectangles/RectanglesTask.cs
+++ Rectangles/RectanglesTask.cs
@@ -4,17 +4,29 @@ namespace Rectangles
 {
 	public static class RectanglesTask
 	{
+		public static int i = 0;
 		// Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
 		public static bool AreIntersected(Rectangle r1, Rectangle r2)
 		{
-			// так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
-			return true;
+			return r1.Left <= r2.Right && r1.Right >= r2.Left && r1.Bottom >= r2.Top && r1.Top <= r2.Bottom;
+
 		}
 
 		// Площадь пересечения прямоугольников
 		public static int IntersectionSquare(Rectangle r1, Rectangle r2)
 		{
-			return 0;
+			bool intersects = AreIntersected(r1, r2);
+
+			if (!intersects)
+				return 0;
+			
+			int[] xarr = { r1.Left, r1.Right, r2.Left, r2.Right };
+			int[] yarr = { r1.Top, r1.Bottom, r2.Top, r2.Bottom };
+
+			Array.Sort(xarr);
+			Array.Sort(yarr);
+
+			return (xarr[2] - xarr[1]) * (yarr[2] - yarr[1]);
 		}
 
 		// Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
@@ -22,6 +34,7 @@ namespace Rectangles
 		// Если прямоугольники совпадают, можно вернуть номер любого из них.
 		public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
 		{
+			r1.Left <= r2.Right && r1.Right >= r2.Left && r1.Bottom >= r2.Top && r1.Top <= r2.Bottom;
 			return -1;
 		}
 	}
