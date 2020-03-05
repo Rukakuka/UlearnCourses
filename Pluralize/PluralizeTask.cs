fd7b66481ea6bf7ca7ef0a5779d16c4707cfa589
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-05 20:31:12 +1000

    Update

---
+++ Pluralize/PluralizeTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			var mod100 = count % 100;
+			var mod10 = mod100 % 10;
+
+			if (mod10 == 1 && mod100 != 11)
+			{
+				return "рубль";
+			}
+			if (mod10 > 1 && mod10 < 5 && (mod100 < 12 || mod100 > 15))
+			{
+				return "рубля";
+			}
+			return "рублей";
+		}
+	}
+}
