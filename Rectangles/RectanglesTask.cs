f0a7dbbfe79950ea4a40b207262e14e27a9c6def
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-06 14:07:24 +1000

    Update

--- Rectangles/RectanglesTask.cs
+++ Rectangles/RectanglesTask.cs
@@ -4,12 +4,10 @@ namespace Rectangles
 {
 	public static class RectanglesTask
 	{
-		public static int i = 0;
 		// Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
 		public static bool AreIntersected(Rectangle r1, Rectangle r2)
 		{
 			return r1.Left <= r2.Right && r1.Right >= r2.Left && r1.Bottom >= r2.Top && r1.Top <= r2.Bottom;
-
 		}
 
 		// Площадь пересечения прямоугольников
@@ -34,8 +32,17 @@ namespace Rectangles
 		// Если прямоугольники совпадают, можно вернуть номер любого из них.
 		public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
 		{
-			r1.Left <= r2.Right && r1.Right >= r2.Left && r1.Bottom >= r2.Top && r1.Top <= r2.Bottom;
+			if (CheckInner(r2, r1))
+				return 0;
+			if (CheckInner(r1, r2))
+				return 1;
 			return -1;
+
+			bool CheckInner(Rectangle R1, Rectangle R2) => // lambda
+				(R2.Left >= R1.Left &&
+				 R2.Right <= R1.Right &&
+				 R2.Bottom <= R1.Bottom &&
+				 R2.Top >= R1.Top);
 		}
 	}
 }
