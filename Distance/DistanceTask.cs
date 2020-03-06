bc8b9de770acb0efaddae38e3777a94a52a5ab41
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-06 14:50:34 +1000

    Update

--- Distance/DistanceTask.cs
+++ Distance/DistanceTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace DistanceTask
 {
@@ -7,7 +8,9 @@ namespace DistanceTask
 		// Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
 		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
 		{
-			return 0.0;
+			double t = ((x-ax)*(bx-ax)+(y-ay)*(by-ay)) / (Math.Pow(bx-ax,2) + Math.Pow(by-ay,2));
+			t = t < 0 ? 0 : t < 1 ? t : 1;
+			return Math.Sqrt(Math.Pow(ax-x+t*(bx-ax),2) + Math.Pow(ay - y + t * (by - ay), 2));
 		}
 	}
 }
