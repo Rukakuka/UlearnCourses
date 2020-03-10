84e28d6a086b40aa9802a88bd5c6fe8cd771be8b
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-10 12:50:17 +1000

    Update

--- Mazes/DiagonalMazeTask.cs
+++ Mazes/DiagonalMazeTask.cs
@@ -1,9 +1,41 @@
-﻿namespace Mazes
+﻿using System;
+
+namespace Mazes
 {
-	public static class DiagonalMazeTask
-	{
-		public static void MoveOut(Robot robot, int width, int height)
-		{
-		}
-	}
+    public static class DiagonalMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            Direction dir = WhatFirstMove(width, height);
+            int bigStep = (int)Math.Round((double)Math.Max(width, height) / Math.Min(width, height));
+
+            for (int i = 0; i < Math.Min(width, height) - 3; i++)
+            {
+                Move(robot, bigStep, dir);
+                dir = Turn(dir);
+                Move(robot, 1, dir);
+                dir = Turn(dir);
+            }
+            Move(robot, bigStep, dir);
+        }
+
+        private static void Move(Robot robot, int steps, Direction direction)
+        {
+            for (int i = 0; i < steps; i++)
+                robot.MoveTo(direction);
+        }
+        private static Direction WhatFirstMove(int width, int height)
+        {
+            if (width > height)
+                return Direction.Right;
+            else
+                return Direction.Down;
+        }
+        private static Direction Turn(Direction direction)
+        {
+            if (direction == Direction.Down) return Direction.Right;
+            if (direction == Direction.Right) return Direction.Down;
+            throw new NotImplementedException();
+        }
+    }
 }
--- Mazes/PyramidMazeTask.cs
+++ Mazes/PyramidMazeTask.cs
@@ -4,18 +4,7 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-			Direction currentDirection = Direction.Right;
-			for (int i = 0; i < (height - 2) / 2; i += 1)
-			{
-				Move(robot, width - 3, currentDirection);
-				Move(robot, 2, Direction.Down);
-			}
-			Move(robot, width - 3, currentDirection);
-		}
-		private static void Move(Robot robot, int steps, Direction direction)
-		{
-			for (int i = 0; i < steps; i++)
-				robot.MoveTo(direction);
+			throw new System.NotImplementedException();
 		}
 	}
 }
