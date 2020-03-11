2d8cb61dff73e55a250f09f93fecc6359e58bdc7
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-11 17:44:32 +1000

    Update

---
+++ Names/HeatmapTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
+                new[] { "a", "b", "c", "d" }, 
+                new[] { "X", "Y", "Z" });
+        }
+    }
+}
---
+++ Names/HistogramTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), 
+                new string[0], 
+                new double[0]);
+        }
+    }
+}
