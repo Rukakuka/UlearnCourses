279789f65cd7191aec30df1ef7127be20e92a182
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-11 22:34:51 +1000

    Update

--- Names/HeatmapTask.cs
+++ Names/HeatmapTask.cs
@@ -6,11 +6,30 @@ namespace Names
     {
         public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
         {
+            string[] xLabels = new string[31];
+            for (int i = 0; i < xLabels.Length; i++)
+                xLabels[i] = (i + 1).ToString();
+
+            string[] yLabels = new string[12];
+            for (int i = 0; i < yLabels.Length; i++)
+                xLabels[i] = (i + 1).ToString();
+
+            var data = new double[31,12];
+            for (int i = 0; i < data.GetLength(0); i++)
+                for (int j = 0; j < data.GetLength(1); j++)
+                    data[i, j] = 1;
+            data[0, 0] = 2;
             return new HeatmapData(
-                "Пример карты интенсивностей",
-                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-                new[] { "a", "b", "c", "d" }, 
-                new[] { "X", "Y", "Z" });
+               "Пример карты интенсивностей",
+               data,
+               xLabels,
+               yLabels);
+
+            //return new HeatmapData(
+            //    "Пример карты интенсивностей",
+            //    new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
+            //    new[] { "a", "b", "c", "d" }, 
+            //    new[] { "X", "Y", "Z" });
         }
     }
 }
--- Names/HistogramTask.cs
+++ Names/HistogramTask.cs
@@ -7,10 +7,20 @@ namespace Names
     {
         public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
         {
+            string[] xLabels = new string[31];
+            for (int i = 0; i < xLabels.Length; i++)
+                xLabels[i] = (i + 1).ToString();
+
+            double[] yData = new double[31];
+            foreach (var man in names)
+                if (man.Name.Equals(name) && man.BirthDate.Day != 1)
+                    yData[man.BirthDate.Day - 1]++;
+
+
             return new HistogramData(
                 string.Format("Рождаемость людей с именем '{0}'", name), 
-                new string[0], 
-                new double[0]);
+                xLabels, 
+                yData);
         }
     }
 }
