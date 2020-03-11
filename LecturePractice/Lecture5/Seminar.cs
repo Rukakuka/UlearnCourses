322da39768f8c17f6543ec676d0e13228c44a61c
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-11 23:10:32 +1000

    Update

---
+++ LecturePractice/Lecture5/Seminar.cs
@@ -0,0 +1,18 @@
+﻿// <Summary>
+// Задачи на семинар
+// Эти задачи можно использовать для самостоятельного решения или для решения на семинарах. Пожалуйста,
+// не публикуйте решения задач в комментариях.
+// </Summary>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace LecturePractice.Lecture5
+{
+    public static class Seminar
+    {
+
+    }
+}
--- Names/HeatmapTask.cs
+++ Names/HeatmapTask.cs
@@ -6,30 +6,24 @@ namespace Names
     {
         public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
         {
-            string[] xLabels = new string[31];
+            string[] xLabels = new string[30];
             for (int i = 0; i < xLabels.Length; i++)
-                xLabels[i] = (i + 1).ToString();
-
+                xLabels[i] = (i+2).ToString();
+            
             string[] yLabels = new string[12];
             for (int i = 0; i < yLabels.Length; i++)
-                xLabels[i] = (i + 1).ToString();
+                yLabels[i] = (i + 1).ToString();
 
-            var data = new double[31,12];
-            for (int i = 0; i < data.GetLength(0); i++)
-                for (int j = 0; j < data.GetLength(1); j++)
-                    data[i, j] = 1;
-            data[0, 0] = 2;
+            var data = new double[30,12];
+            foreach (var man in names)
+                if (man.BirthDate.Day != 1)
+                    data[man.BirthDate.Day - 2, man.BirthDate.Month - 1]++;
+
             return new HeatmapData(
                "Пример карты интенсивностей",
                data,
                xLabels,
                yLabels);
-
-            //return new HeatmapData(
-            //    "Пример карты интенсивностей",
-            //    new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            //    new[] { "a", "b", "c", "d" }, 
-            //    new[] { "X", "Y", "Z" });
         }
     }
 }
