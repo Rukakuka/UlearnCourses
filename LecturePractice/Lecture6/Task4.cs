fa16562413cf6160dc16ca600caf330b74295cb6
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-16 17:44:12 +1000

    Update

---
+++ LecturePractice/Lecture6/Task4.cs
@@ -0,0 +1,15 @@
+﻿// <Summary>
+// 
+// </Summary>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LecturePractice.Lecture6
+{
+    public static class Task4
+    {
+
+    }
+}
