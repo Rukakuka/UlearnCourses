7270eb58f22f97ba2cba4e8c0a18f1be6b30cdcf
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-03-17 20:30:41 +1000

    Update

---
+++ TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            //...
+            return sentencesList;
+        }
+    }
+}
